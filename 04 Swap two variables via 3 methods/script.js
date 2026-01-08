919a1cbdb99e00eca62c673cd7a16a38ebd03c44
Vivek Kr <vivekkrdas03@gmail.com>
2026-01-08 14:13:52 +0530

    Swapping without using a third variable

--- 04 Swap two variables via 3 methods/script.js
+++ 04 Swap two variables via 3 methods/script.js
@@ -11,3 +11,20 @@ a = b; // a = 20, b = 20
 b = temp; // b = 10
 
 console.log(`After swapping: a = ${a}, b = ${b}\n`);
+
+
+// Method 2: Swapping without using a third variable
+{
+  let a = 5;
+  let b = 7;
+
+  console.log(`Before swapping: a = ${a}, b = ${b}`);
+
+  // Swap Logic
+  // (initially a = 5, b = 7)
+  a = a + b; // a = 12, b = 7
+  b = a - b; // b = 5, a = 12
+  a = a - b; // a = 7, b = 5
+
+  console.log(`After swapping: a = ${a}, b = ${b}`);
+}
